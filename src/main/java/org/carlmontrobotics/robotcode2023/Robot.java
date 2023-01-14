c744b972b0cef2d19c921a0d834eb47407849c17
CoolSpy3 <coolspythree@gmail.com>
2023-01-14 14:48:13 -0800

    change package name

--- src/main/java/org/carlmontrobotics/robotcode2023/Robot.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/Robot.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.carlmontrobotics.robotcode2023;
 
 import org.carlmontrobotics.lib199.MotorErrors;
 
