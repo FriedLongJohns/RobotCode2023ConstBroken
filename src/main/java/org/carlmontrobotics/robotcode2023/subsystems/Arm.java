8cb7eca04de665edc4fcaa4d32f8e787350db98b
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2023-02-04 13:20:14 -0800

    ArmFeedForward + enum + QOL
    =change armPos() to two funcs, snappedArmPos() and closeSnappedArmPos() - snapped arm pos does not care about error whereas closeSnapped returns ArmPos.ERROR enum if arm is too far way from one of the 3 positions
    +add ArmPos enum with LOW,MID,HIGH,ERROR and numeric values affectable by smartDashboard
    +Use ArmFeedForward from WPILIB, needs testing
    =sort imports
    +ArmPos enum has .next() for easy arm cycling
    
    TODO
    test if ArmFeedForward does what it should
    test everything for good numbers
    assign the 60:1 gear ratio to the arm motors

--- src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
@@ -1,56 +1,132 @@
 package org.carlmontrobotics.robotcode2023.subsystems;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.carlmontrobotics.lib199.MotorControllerFactory;
 import org.carlmontrobotics.lib199.MotorErrors.TemperatureLimit;
 import org.carlmontrobotics.robotcode2023.Constants;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
+
 public class Arm extends SubsystemBase
 {
-    public CANSparkMax motorL = MotorControllerFactory.createSparkMax(Constants.Arm.portL, TemperatureLimit.NEO);
-    public CANSparkMax motorR = MotorControllerFactory.createSparkMax(Constants.Arm.portR, TemperatureLimit.NEO);
-    public RelativeEncoder motorLencoder = motorL.getEncoder();
-    public RelativeEncoder motorRencoder = motorR.getEncoder();
+  public CANSparkMax motorL = MotorControllerFactory.createSparkMax(Constants.Arm.portL, TemperatureLimit.NEO);
+  public CANSparkMax motorR = MotorControllerFactory.createSparkMax(Constants.Arm.portR, TemperatureLimit.NEO);
+  public RelativeEncoder motorLencoder = motorL.getEncoder();
+  public RelativeEncoder motorRencoder = motorR.getEncoder();
 
-    public double encoderErrorTolerance = .05;
+  public double encoderErrorTolerance = .05;
 
-    
+  private double kS = .2; //volts | base speed
+  private double kG = .1; //volts | gravity... something
+  private double kV = .2; //volts*secs/rad | extra velocity
+  private double kA = .3; //volts*secs^2/rad | vacceleration
+  /// these are all units ^ , actual arm speed is determined by values in .calculate
+  private double FFvelocity = 2;
+  private double FFaccel = 1;
+  private ArmFeedforward armFeed = new ArmFeedforward(kS,kG,kV,kA);
+
+  public double goalPos;
 
 
 
-    //MotorL is the leader btw
-    public Arm(){
+  //MotorL is the leader btw
+  public Arm(){
     motorR.follow(motorL, true);
+    //TODO: Assign the 60:1 gear ratio to the motors
 
-    SmartDashboard.putNumber("EncoderStartPosition", 0);
-    SmartDashboard.putNumber("EncoderMidPosition", 0.2);
-    SmartDashboard.putNumber("EncoderHighPosition", 0.4);
+    SmartDashboard.putNumber("EncoderLowPos", ArmPos.LOW.value);
+    SmartDashboard.putNumber("EncoderMidPos", ArmPos.MID.value);
+    SmartDashboard.putNumber("EncoderHighPos", ArmPos.HIGH.value);
 
-    
-    
+    SmartDashboard.putNumber("FeedForward:kS", kS);
+    SmartDashboard.putNumber("FeedForward:kG", kG);
+    SmartDashboard.putNumber("FeedForward:kV", kV);
+    SmartDashboard.putNumber("FeedForward:kA", kA);
+    SmartDashboard.putNumber("FF: Velocity", FFvelocity);
+    SmartDashboard.putNumber("FF: Acceleration", FFaccel);
+
+    SmartDashboard.putNumber("GoalPosition", goalPos);
+  }
+
+  @Override
+  public void periodic() {
+    //you can do this because enum is just a class :)
+    ArmPos.LOW.value = SmartDashboard.getNumber("EncoderLowPos", ArmPos.LOW.value);
+    ArmPos.MID.value = SmartDashboard.getNumber("EncoderMidPos", ArmPos.MID.value);
+    ArmPos.HIGH.value = SmartDashboard.getNumber("EncoderHighPos", ArmPos.HIGH.value);
+
+    kS = SmartDashboard.getNumber("FeedForward: kS", kS);
+    kG = SmartDashboard.getNumber("FeedForward: kG", kG);
+    kV = SmartDashboard.getNumber("FeedForward: kV", kV);
+    kA = SmartDashboard.getNumber("FeedForward: kA", kA);
+    FFvelocity = SmartDashboard.getNumber("FF: Velocity", FFvelocity);
+    FFaccel = SmartDashboard.getNumber("FF: Acceleration", FFaccel);
+
+    goalPos = SmartDashboard.getNumber("GoalPosition", goalPos);//pls no touch while arm is cycle-ing
+
+
+    double difference = goalPos-motorLencoder.getPosition();
+    if (difference>encoderErrorTolerance){//even PID needs an acceptable error sometimes
+      //assuming calculate() is some sort of PID-esque thing
+      motorL.set(armFeed.calculate(difference, FFvelocity, FFaccel));
     }
-   
-    //Tests if possition in right or wrong
-    public String armPosition(){
-        
-        double EncoderPosition = motorRencoder.getPosition();
-    
-        if(EncoderPosition<SmartDashboard.getNumber("EncoderStartPosition",-1)+encoderErrorTolerance &&  EncoderPosition>SmartDashboard.getNumber("EncoderStartPosition",-1)-encoderErrorTolerance){
-            return "Start";
-        } else if(EncoderPosition<SmartDashboard.getNumber("EncoderMidPosition",-1)+encoderErrorTolerance && 
-        EncoderPosition>SmartDashboard.getNumber("EncoderMidPosition",-1)-encoderErrorTolerance){
-            return "Mid";
-        } else if(EncoderPosition<SmartDashboard.getNumber("EncoderHighPosition",-1)+encoderErrorTolerance && 
-        EncoderPosition>SmartDashboard.getNumber("EncoderHighPosition",-1)-encoderErrorTolerance){
-            return "High";
-        } else {
-            return "Error";
-        }
-       
-    
+  }
+
+  //Snaps raw encoder pos to one of our cycle positions
+  public ArmPos snappedArmPos(){
+    double encoderPos = motorLencoder.getPosition();
+
+    if(encoderPos>=ArmPos.MHH.value){//greater than halfway between mid and high
+      return ArmPos.HIGH;
+    } if(encoderPos<=ArmPos.LMH.value){//lass than halfway between low and mid
+      return ArmPos.LOW;
+    } else{//we are (essentially) at half point
+      return ArmPos.MID;
     }
+  }
+
+  public ArmPos closeSnappedArmPos(){//more precise snapping
+    double encoderPos = motorLencoder.getPosition();
+
+    if (Math.abs(ArmPos.LOW.value-encoderPos)>encoderErrorTolerance){
+      return ArmPos.LOW;
+    } else if (Math.abs(ArmPos.MID.value-encoderPos)>encoderErrorTolerance){
+      return ArmPos.MID;
+    } else if (Math.abs(ArmPos.HIGH.value-encoderPos)>encoderErrorTolerance){
+      return ArmPos.HIGH;
+    } else {
+      return ArmPos.ERROR;
+    }
+  }
+
+  public void cyclePosition(){//because most people won't remember/want to do this long function chain
+    goalPos = snappedArmPos().next().value;
+  }
+
+  public enum ArmPos{
+    LOW(0),MID(.2),HIGH(.4),ERROR(-1),LMH(.1),MHH(.3);
+    //Low-Mid Halfpoint and Mid-High Halfpoint are for easy position snapping math
+
+    public double value;
+    ArmPos(double value){
+      this.value=value;
+    }
+
+    public ArmPos next(){
+      switch (this){
+        case LOW:
+          return ArmPos.MID;
+        case MID:
+          return ArmPos.HIGH;
+        case HIGH:
+          return ArmPos.LOW;
+        default:
+          return this;
+      }
+    }
+  }
 }
-
