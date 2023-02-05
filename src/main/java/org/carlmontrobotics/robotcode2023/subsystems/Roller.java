58b3d7edec70fcde776cac1ecf08513fad9f5f6c
CoolSpy3 <coolspythree@gmail.com>
2023-02-05 14:16:54 -0800

    use percent control

--- src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Roller.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Roller.java
@@ -19,51 +19,44 @@ import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Roller extends SubsystemBase {
 
     private final CANSparkMax motor = MotorControllerFactory.createSparkMax(Constants.ROLLER_PORT, TemperatureLimit.NEO_550);
 
-    public static final double coneIntakeConeOuttakeSpeed = 60;
-    public static final double coneOuttakeConeIntakeSpeed = 60;
+    public static final double coneIntakeConeOuttakeSpeed = .1;
+    public static final double coneOuttakeConeIntakeSpeed = -.1;
 
     private final AddressableLED led = new AddressableLED(8);
     private final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(84);
-    private final SparkVelocityPIDController pidController = new SparkVelocityPIDController("Roller Motor", motor, 0, 0, 0, 0.2317, 0.061774, 0, 10);
+    private double maxCurrent = 100;
     private boolean motorInverted = false;
 
     public Roller() {
         led.setLength(ledBuffer.getLength());
+        // led.setData(ledBuffer);
+        setLedColor(Color.CYAN);
         led.start();
-        LiveWindow.enableTelemetry(pidController);
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        SmartDashboard.putNumber("Roller Current", motor.getOutputCurrent());
+    }
 
     public void setSpeed(double speed) {
-        motor.getEncoder().setVelocityConversionFactor(Math.copySign(1, speed));
-        boolean requestedInversion = speed < 0;
-        if(requestedInversion != motorInverted) {
-            motorInverted = requestedInversion;
-            motor.setInverted(motorInverted);
-        }
-        pidController.setTargetSpeed(Math.abs(speed));
+        motor.set(speed);
     }
 
     public void setLedColor(Color color) {
         for(int i = 0; i < ledBuffer.getLength(); i++)
             ledBuffer.setRGB(i, color.getRed(), color.getGreen(), color.getBlue());
+        led.setData(ledBuffer);
     }
 
     public boolean hasGamePiece() {
-        return !pidController.isAtTargetSpeed();
-    }
-
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        super.initSendable(builder);
-        SendableRegistry.addChild(this, pidController);
+        return motor.getOutputCurrent() > maxCurrent;
     }
 }
