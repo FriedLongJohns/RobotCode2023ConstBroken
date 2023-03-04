4733ef4a4cdc76a6335537c6958c8b449015cf4c
RayaSkaf <114116795+RayaSkaf@users.noreply.github.com>
2023-03-04 01:02:32 +0000

    raya comment changes and resolves

--- src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
@@ -5,143 +5,96 @@ import org.carlmontrobotics.lib199.MotorErrors.TemperatureLimit;
 import org.carlmontrobotics.robotcode2023.Constants;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.SparkMaxPIDController;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
 
-public class Arm extends SubsystemBase
-{
+public class Arm extends SubsystemBase {
+  
   public CANSparkMax motor = MotorControllerFactory.createSparkMax(Constants.Arm.port, TemperatureLimit.NEO);
-  public RelativeEncoder motorLencoder = motor.getEncoder();
+  public static CANSparkMax motorL = MotorControllerFactory.createSparkMax(Constants.Arm.portL, TemperatureLimit.NEO);
+  public CANSparkMax motorR = MotorControllerFactory.createSparkMax(Constants.Arm.portR, TemperatureLimit.NEO);
+  private final RelativeEncoder motorLencoder = motorL.getEncoder();
+  private final RelativeEncoder motorRencoder = motorR.getEncoder();
 
-  public double encoderErrorTolerance = .1;
+  public double encoderErrorTolerance = .05;
 
-  private double kS = .17764; //volts | base speed
-  private double kG = 4.1181; //volts | gravity... something
-  private double kV = 1.7912; //volts*secs/rad | extra velocity
-  private double kA = .15225; //volts*secs^2/rad | vacceleration
-  /// these are all units ^ , actual arm0.15225 speed is determined by values in .calculate
-  private double Kp = 7.2985;
-  private double Kd = 2.2943;
-  private double Ki = 0.0;
-  
-  private double setpoint = 2.2;
-  private double FFvelocity = 6;
-  private double FFaccel = 6;
+  private double kS = .067766; //volts | base speed
+  private double kG = .0075982; //volts | gravity... something
+  private double kV = .019762; //volts*secs/rad | extra velocity
+  private double kA = .00039212; //volts*secs^2/rad | vacceleration
+  /// these are all units ^ , actual arm speed is determined by values in .calculate
+  private double FFvelocity = .01;
+  private double FFaccel = .01;
   private ArmFeedforward armFeed = new ArmFeedforward(kS, kG, kV, kA);
-  private PIDController pid = new PIDController(Kp, Ki, Kd);
-  
+
   public double goalPos;
 
   public enum ArmPreset {
-    INTAKE(0), MID(1.5), HIGH(2);
+    INTAKE(0.31), MID(-1.74), HIGH(-1.83);
     
     public double value; //not static so SmartDashboard can touch [IMPORTANT TO KNOW!]
     ArmPreset(double value) {
       this.value = value;
     }
-    public String toString() {
-      switch (this) {
-        case INTAKE: return "INTAKE";
-        case MID: return "MID";
-        case HIGH: return "HIGH";
-      }
-      return "null";
-    }
     public ArmPreset next() {
       switch (this) {
         case INTAKE: return MID;
+
         case MID: return HIGH;
         case HIGH: return INTAKE;
       }
       return null;
     }
-    public ArmPreset prev(){
-      switch (this) {
-        case INTAKE: return HIGH;
-        case MID: return INTAKE;
-        case HIGH: return MID;
-      }
-      return null;
-    }
-    
   }
 
-  public Arm(){
-    motorLencoder.setPositionConversionFactor(2*Math.PI/100);
-    motorLencoder.setPosition(-Math.PI / 2);
+  public Arm() {
+    motorLencoder.setPositionConversionFactor(1/60);
+    motorLencoder.setPosition(0.0);
     SmartDashboard.putNumber("FF: Velocity", FFvelocity);
     SmartDashboard.putNumber("FF: Acceleration", FFaccel);
     SmartDashboard.putNumber("GoalPosition", goalPos);
-    SmartDashboard.putNumber("KG", kG);
-    SmartDashboard.putString("Debooog", "No.");
-    SmartDashboard.putString("ArmENUM", 
-    snappedArmPos().toString()
-    );
-    SmartDashboard.putNumber("KP", Kp);
-    SmartDashboard.putNumber("KI", Ki);
-    SmartDashboard.putNumber("KD", Kd);
-    pid.setTolerance(2.5,10);
   }
 
   @Override
   public void periodic() {
     FFvelocity = SmartDashboard.getNumber("FF: Velocity", FFvelocity);
-    SmartDashboard.getNumber("KG", kG);
     FFaccel = SmartDashboard.getNumber("FF: Acceleration", FFaccel);
     goalPos = SmartDashboard.getNumber("GoalPosition", goalPos);
     SmartDashboard.putNumber("ArmLencoderPos", motorLencoder.getPosition());
-    SmartDashboard.putString("ArmENUM", 
-    (closeSnappedArmPos() != null) ? closeSnappedArmPos().toString() : snappedArmPos().toString()
-    );
-    Kp = SmartDashboard.getNumber("KP", Kp);
-    Ki = SmartDashboard.getNumber("KI", Ki);
-    Kd = SmartDashboard.getNumber("KD", Kd);
-    pid.setP(Kp);
-    pid.setI(Ki);
-    pid.setD(Kd);
-    double currentPos = motorLencoder.getPosition();
-    //pid.atSetpoint();
 
-    // motor.set(pid.calculate( motorLencoder.getPosition(), setpoint));
-    //FIXME CLAMP LIMIT FOR PROTOTYPE ONLY
-    goalPos = MathUtil.clamp(goalPos, -Math.PI*1.4, -Math.PI*.5);
-    //                           -4.39,  -1.57
-    
-      motor.setVoltage(armFeed.calculate(currentPos, 0, 0)
-         + pid.calculate(currentPos, goalPos));
-         
-     
-    
-    
+    double difference = goalPos - motorLencoder.getPosition();
+    if (difference > encoderErrorTolerance) {//even PID needs an acceptable error sometimes
+      //assuming calculate() is some sort of PID-esque thing
+      motor.set(armFeed.calculate(difference, FFvelocity, FFaccel));
+    }
   }
 
   //Snaps raw encoder pos to one of our cycle positions
-  public ArmPreset snappedArmPos(){
+  public ArmPreset snappedArmPos() {
     double encoderPos = motorLencoder.getPosition();
-    double bestD = Double.MAX_VALUE;
-    ArmPreset bestE = closeSnappedArmPos();
-    for(ArmPreset check : ArmPreset.values()){
-      double dist = Math.abs(check.value - encoderPos);
-      if (dist < bestD){
-        bestD = dist;
-        bestE = check;
+    
+    for(ArmPreset check : ArmPreset.values()) {
+      double lowdist = (check.value - check.prev().value) / 2;
+      double hidist = (check.next().value - check.value) / 2; // get the halfway points between each position and it's neighbors
+      if (check.value - lowdist < encoderPos && encoderPos < check.value + hidist) {
+        //seperate high and low instead of ABS because maybe difference isn't constant between each position of arm
+          //and yes it still works for lowest and highest value
+          return check;
       }
     }
-    return bestE;
+    //help something went wrong
+    return null;
   }
 
   public ArmPreset closeSnappedArmPos() {//more precise snapping
     double encoderPos = motorLencoder.getPosition();
     
-    for(ArmPreset check : ArmPreset.values()){
-        if (Math.abs(check.value - encoderPos) < encoderErrorTolerance) {
+    for(ArmPreset check : ArmPreset.values()) {
+        if (Math.abs(check.value - encoderPos) > encoderErrorTolerance) {//maybe will break if cone/cube values are close, but if they are close then lower error or only use one enum
             return check;
         }
     }
@@ -149,24 +102,20 @@ public class Arm extends SubsystemBase
     return null;
   }
 
-  public void cycleUp(){ 
+  public void cycleUp() { 
     // because most people won't remember/want to do this long function chain
-    SmartDashboard.putString("Debooog", "CycleUp");
     SmartDashboard.putNumber("GoalPosition", 
-    closeSnappedArmPos() != null ? closeSnappedArmPos().next().value : snappedArmPos().next().value
-    );
+    closeSnappedArmPos() != null ? closeSnappedArmPos().next().value : snappedArmPos().next().value);
     // if closeSnappedArmPos is working, swap based on it - otherwise use less accurate snapping
   }
   
-  public void cycleDown(){
-    SmartDashboard.putString("Debooog", "CycleDown");
+  public void cycleDown() {
     SmartDashboard.putNumber("GoalPosition", 
     closeSnappedArmPos() != null ? closeSnappedArmPos().prev().value : snappedArmPos().prev().value
     );
   }
   
-  public void setPreset(ArmPreset preset){
+  public void setPreset(ArmPreset preset) {
     SmartDashboard.putNumber("GoalPosition", preset.value);
   }
 }
-
