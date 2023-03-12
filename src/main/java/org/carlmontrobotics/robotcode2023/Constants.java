e10624135bfd4cdd09e25419bdde61d18aaef72d
CoolSpy3 <coolspythree@gmail.com>
2023-03-12 16:56:23 -0700

    code cleanup and bug fixes

--- src/main/java/org/carlmontrobotics/robotcode2023/Constants.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/Constants.java
@@ -4,8 +4,9 @@
 
 package org.carlmontrobotics.robotcode2023;
 
-import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController.Button;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -23,12 +24,57 @@ public final class Constants {
 
     public static final double grabber_open_position = 2.8;
     public static final double grabber_closed_position = -6.9;
-    public static final class Arm{
-        public static final int port = 17;
-        //2 for prototype
+    public static final class Arm {
+
+        //#region Subsystem Constants
+
+        // Feedforward
+        // FIXME WRIST NEEDS SYSID DONE
+        // Arm, Wrist
+        public static final double[] kS = { .067766, .074798 }; // (V)
+        public static final double[] kV = { .019762, 1.6743 }; // (V / rad/s)
+        public static final double[] kA = { .00039212, 0.032177 }; // (V / rad/s^2)
+        public static final double kG_WRIST = .36214; // (V)
+
+        // PID
+        // FIXME BOTH WRIST AND ARM NEED PID DONE
+        // Arm, Wrist
+        public static double[] kP = { 0, 0 }; // (V / rad)
+        public static double[] kI = { 0, 0 }; // (V / (rad * s) )
+        public static double[] kD = { 0, 0 }; // (V / (rad / s) )
+
+        // Arm, Wrist
+        public static double[] posToleranceRad = { .05, .05 }; // rad
+        public static double[] velToleranceRadPSec = { 0.5, 0.5 }; // rad/s
+
+        public static double[] goalPosRad = { -Math.PI / 2, 0 }; // rad
+        public static double[] offsetRad = { 2.08, 4.02 }; // rad
+
+        // needed to calculate feedforward values dynamically
+        public static final double ARM_MASS_KG = Units.lbsToKilograms(6.57);
+        public static final double ARM_LENGTH_METERS = Units.inchesToMeters(38.25);
+
+        // Distance from the arm motor to the center of mass of the  arm
+        public static final double COM_ARM_LENGTH_METERS = Units.inchesToMeters(13.23);
+        public static final double ROLLER_MASS_KG = Units.lbsToKilograms(10.91);
+
+        // distance of center of mass of roller to the WRIST motor
+        public static final double COM_ROLLER_LENGTH_METERS = Units.inchesToMeters(9.47);
+        public static final double g = 9.81;
+
+        public static final double V_PER_NM = 0;
+
+        //#endregion
+
+        //#region Ports
+
+        public static final int armMotorPort = 17;
+        public static final int wristMotorPort = 19;
+
+        //#endregion
+
     }
     public static final class Wrist {
-        public static final int port = 19;
     }
 
     public static final class OI {
--- src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
+++ src/main/java/org/carlmontrobotics/robotcode2023/subsystems/Arm.java
@@ -1,71 +1,41 @@
 package org.carlmontrobotics.robotcode2023.subsystems;
 
+import static org.carlmontrobotics.robotcode2023.Constants.Arm.*;
+
 import org.carlmontrobotics.MotorConfig;
 import org.carlmontrobotics.lib199.MotorControllerFactory;
-import org.carlmontrobotics.robotcode2023.Constants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// All angles are with 0 representing the arm/wrist being parallel to the floor on the intake side of the robot
+// Arm angle is measured from horizontal on the intake side of the robot and bounded between -3π/2 and π/2
+// Wrist angle is measured relative to the arm with 0 being parallel to the arm and bounded between -π and π
 public class Arm extends SubsystemBase {
 
-    private CANSparkMax armMotor = MotorControllerFactory.createSparkMax(Constants.Arm.port, MotorConfig.NEO);
-    private CANSparkMax wristMotor = MotorControllerFactory.createSparkMax(Constants.Wrist.port, MotorConfig.NEO);
+    // Array Indexes (Just to make things easier to read)
+    private static final int ARM = 0;
+    private static final int WRIST = 1;
 
-    private SparkMaxAbsoluteEncoder armEncoder = armMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
-    private SparkMaxAbsoluteEncoder wristEncoder = wristMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
+    private final CANSparkMax armMotor = MotorControllerFactory.createSparkMax(armMotorPort, MotorConfig.NEO);
+    private final CANSparkMax wristMotor = MotorControllerFactory.createSparkMax(wristMotorPort, MotorConfig.NEO);
 
-    // FIXME WRIST NEEDS SYSID DONE
-    // kS (V), kG (V) (Wrist Only), kV (V/(rad/s)), kA (V/(rad/s^2))
-    private final double[] armFeedConsts = { .067766, .019762, .00039212 };
-    private final double[] wristFeedConsts = { .074798, 0.36214, 1.6743, 0.032177 };
+    private final SparkMaxAbsoluteEncoder armEncoder = armMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
+    private final SparkMaxAbsoluteEncoder wristEncoder = wristMotor.getAbsoluteEncoder(SparkMaxAbsoluteEncoder.Type.kDutyCycle);
 
-    private double[] ffVelocityRadPSec = { 0, 0 }; // rad/s
-    private double[] ffAccelRadPSec2 = { 0, 0 }; // rad/s^2
+    private final SimpleMotorFeedforward armFeed = new SimpleMotorFeedforward(kS[ARM], kV[ARM], kA[ARM]);
+    private final ArmFeedforward wristFeed = new ArmFeedforward(kS[WRIST], kG_WRIST, kV[WRIST], kA[WRIST]);
 
-    private SimpleMotorFeedforward armFeed = new SimpleMotorFeedforward(armFeedConsts[0], armFeedConsts[1], armFeedConsts[2]);
-    private ArmFeedforward wristFeed = new ArmFeedforward(wristFeedConsts[0], wristFeedConsts[1], wristFeedConsts[2], wristFeedConsts[3]);
-
-    // PID
-    // FIXME BOTH WRIST AND ARM NEED PID DONE
-    // Arm, Wrist
-    private double[] kP = { 0, 0 }; // (V / rad)
-    private double[] kI = { 0, 0 }; // (V / (rad * s) )
-    private double[] kD = { 0, 0 }; // (V / (rad / s) )
-
-    // Arm, Wrist
-    private double[] posToleranceRad = { .05, .05 }; // rad
-    private double[] velToleranceRadPSec = { 0.5, 0.5 }; // rad/s
-
-    private PIDController armPID = new PIDController(kP[0], kI[0], kD[0]);
-    private PIDController wristPID = new PIDController(kP[1], kI[1], kD[1]);
-
-    private double[] goalPosRad = { -Math.PI / 2, 0 }; // rad
-    private double[] offsetRad = { 2.08, 4.02 }; // rad
-
-    // needed to calculate feedforward values dynamically
-    private final double ARM_MASS_KG = Units.lbsToKilograms(6.57);
-    private final double ARM_LENGTH_METERS = Units.inchesToMeters(38.25);
-
-    // Distance from the arm motor to the center of mass of the  arm
-    private final double COM_ARM_LENGTH_METERS = Units.inchesToMeters(13.23);
-    private final double ROLLER_MASS_KG = Units.lbsToKilograms(10.91);
-
-    // distance of center of mass of roller to the WRIST motor
-    private final double COM_ROLLER_LENGTH_METERS = Units.inchesToMeters(9.47);
-    private final double g = 9.81;
-
-    private final double V_PER_NM = 0;
+    private final PIDController armPID = new PIDController(kP[ARM], kI[ARM], kD[ARM]);
+    private final PIDController wristPID = new PIDController(kP[WRIST], kI[WRIST], kD[WRIST]);
 
     public Arm() {
         armMotor.setInverted(true);
@@ -74,24 +44,32 @@ public class Arm extends SubsystemBase {
         armEncoder.setPositionConversionFactor(2 * Math.PI);
         wristEncoder.setPositionConversionFactor(2 * Math.PI);
 
-        wristEncoder.setZeroOffset(offsetRad[0]);
-        armEncoder.setZeroOffset(offsetRad[1]);
+        wristEncoder.setZeroOffset(offsetRad[ARM]);
+        armEncoder.setZeroOffset(offsetRad[WRIST]);
 
-        armPID.setTolerance(posToleranceRad[0], velToleranceRadPSec[0]);
-        wristPID.setTolerance(posToleranceRad[1], velToleranceRadPSec[1]);
+        armPID.setTolerance(posToleranceRad[ARM], velToleranceRadPSec[ARM]);
+        wristPID.setTolerance(posToleranceRad[WRIST], velToleranceRadPSec[WRIST]);
     }
 
     @Override
     public void periodic() {
-        armMotor.setVoltage(getKg() * getCoM().getAngle().getCos() + armFeed.calculate(0, 0) + armPID.calculate(armEncoder.getPosition(), goalPosRad[0]));
-        wristMotor.setVoltage(wristFeed.calculate(getWristPos(), 0, 0) + wristPID.calculate(getWristPos(), goalPosRad[1]));
+        armMotor.setVoltage(getKg() * getCoM().getAngle().getCos() + armFeed.calculate(0, 0) + armPID.calculate(armEncoder.getPosition(), goalPosRad[ARM]));
+        wristMotor.setVoltage(wristFeed.calculate(getWristPosRelativeToGround(), 0, 0) + wristPID.calculate(getWristPos(), goalPosRad[WRIST]));
+
+        //TODO: REMOVE WHEN DONE WITH TESTING (ANY CODE REVIEWERS, PLEASE REJECT MERGES TO MASTER IF THIS IS STILL HERE)
+        armPID.setP(kP[ARM]);
+        armPID.setI(kI[ARM]);
+        armPID.setD(kD[ARM]);
+        wristPID.setP(kP[WRIST]);
+        wristPID.setI(kI[WRIST]);
+        wristPID.setD(kD[WRIST]);
     }
 
     // distance from center of mass relative to joint holding arm
     public Translation2d getCoM() {
         Translation2d comOfArm = new Translation2d(COM_ARM_LENGTH_METERS, Rotation2d.fromRadians(getArmPos())).times(ARM_MASS_KG);
         Translation2d comOfRoller = new Translation2d(ARM_LENGTH_METERS, Rotation2d.fromRadians(getArmPos()))
-            .plus(new Translation2d(COM_ROLLER_LENGTH_METERS, Rotation2d.fromRadians(getArmPos() + getWristPos()))).times(ROLLER_MASS_KG);
+            .plus(new Translation2d(COM_ROLLER_LENGTH_METERS, Rotation2d.fromRadians(getWristPosRelativeToGround()))).times(ROLLER_MASS_KG);
         return comOfArm.plus(comOfRoller).div(ARM_MASS_KG + ROLLER_MASS_KG);
     }
 
@@ -104,33 +82,44 @@ public class Arm extends SubsystemBase {
     }
 
     public double getArmPos() {
-        return armEncoder.getPosition();
+        return MathUtil.inputModulus(armEncoder.getPosition(), -3 * Math.PI / 2, Math.PI / 2);
     }
 
-    /**
-     * @return The position of the wrist relative to the ground
-     */
     public double getWristPos() {
+        return MathUtil.inputModulus(wristEncoder.getPosition(), -Math.PI, Math.PI);
+    }
+
+    // Unbounded wrist position relative to ground
+    public double getWristPosRelativeToGround() {
         return getArmPos() + wristEncoder.getPosition();
+    }
+
+    public void setArmTarget(double target) {
+        goalPosRad[ARM] = target;
+    }
+
+    public void setWristTarget(double target) {
+        goalPosRad[WRIST] = target;
+    }
+
+    public void setArmWristTarget(double targetArm, double targetWrist) {
+        setArmTarget(targetArm);
+        setWristTarget(targetWrist);
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
-        builder.addDoubleProperty("ArmFFVelocity",() -> ffVelocityRadPSec[0], x -> this.ffVelocityRadPSec[0] = x);
-        builder.addDoubleProperty("WisFFVelocity",() -> ffVelocityRadPSec[0], x -> this.ffVelocityRadPSec[0] = x);
-        builder.addDoubleProperty("ArmFFAccel", () -> ffAccelRadPSec2[0], x -> this.ffAccelRadPSec2[0] = x);
-        builder.addDoubleProperty("WisFFAccel", () -> ffAccelRadPSec2[0], x -> this.ffAccelRadPSec2[0] = x);
-        builder.addDoubleProperty("WristGoalPos", () -> goalPosRad[1], goal -> this.goalPosRad[1] = goal);
+        builder.addDoubleProperty("WristGoalPos", () -> goalPosRad[1], goal -> goalPosRad[1] = goal);
         builder.addDoubleProperty("ArmPos", () -> getArmPos(), null);
-        builder.addDoubleProperty("WisPos", () -> wristEncoder.getPosition(), null);
-        builder.addDoubleProperty("RobotWisPos", () -> getWristPos(), null);
-        builder.addDoubleProperty("kP: Arm", () -> kP[0], x -> this.kP[0] = x);
-        builder.addDoubleProperty("kP: Wis", () -> kP[1], x -> this.kP[1] = x);
-        builder.addDoubleProperty("kI: Arm", () -> kI[0], x -> this.kI[0] = x);
-        builder.addDoubleProperty("kI: Wis", () -> kI[1], x -> this.kI[1] = x);
-        builder.addDoubleProperty("kD: Arm", () -> kD[0], x -> this.kD[0] = x);
-        builder.addDoubleProperty("kD: Wis", () -> kD[1], x -> this.kD[1] = x);
+        builder.addDoubleProperty("WisPos", () -> getWristPos(), null);
+        builder.addDoubleProperty("RobotWisPos", () -> getWristPosRelativeToGround(), null);
+        builder.addDoubleProperty("kP: Arm", () -> kP[ARM], x -> kP[ARM] = x);
+        builder.addDoubleProperty("kP: Wis", () -> kP[WRIST], x -> kP[WRIST] = x);
+        builder.addDoubleProperty("kI: Arm", () -> kI[ARM], x -> kI[ARM] = x);
+        builder.addDoubleProperty("kI: Wis", () -> kI[WRIST], x -> kI[WRIST] = x);
+        builder.addDoubleProperty("kD: Arm", () -> kD[ARM], x -> kD[ARM] = x);
+        builder.addDoubleProperty("kD: Wis", () -> kD[WRIST], x -> kD[WRIST] = x);
     }
 
 }
